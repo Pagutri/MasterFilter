344bc241737a3e33acfd4dd565248c7fc0ba08a1
Pa Gutri <pagutri@gmail.com>
2019-10-28 23:24:50 -0600

    Apariencia final. No branch. A dormir

---
+++ README.txt
@@ -0,0 +1,4 @@
+Nota:
+Para que las notebooks corran tal como se envían, todas
+las imágenes deben estar en una carpeta llamada imagenes.
+No pude subirlas así porque era muy pesado.
--- filtros.py
+++ filtros.py
@@ -31,9 +31,8 @@ def aplicar_filtro(imagen, kernel):
     
     return img_filtrada
 
-def kernel_pasabajos(imagen, d0 = 15, forma = 0, n = 1):
+def kernel_pasabajos(M, N, d0 = 15, forma = 0, n = 1):
     """forma = 0 para kernel ideal, 1 para gaussiano y cualquier otro para Butterworth."""
-    M, N = imagen.shape
     V, U = hacer_meshgrid(M, N)
     # Distancias al cuadrado
     D = np.square(U - 0.5 * M) + np.square(V - 0.5 * N)
@@ -47,23 +46,22 @@ def kernel_pasabajos(imagen, d0 = 15, forma = 0, n = 1):
     
     return kernel
 
-def kernel_pasaaltos(imagen, d0 = 15, forma = 0, n = 1):
+def kernel_pasaaltos(M, N, d0 = 15, forma = 0, n = 1):
     """forma = 0 para kernel ideal, 1 para gaussiano y cualquier otro para Butterworth."""
-    kernel = 1.0 - kernel_pasabajos(imagen, d0, forma, n)
+    kernel = 1.0 - kernel_pasabajos(M, N, d0, forma, n)
     
     return kernel
 
-def kernel_rechazabandas(imagen, c0 = 20, w = 10, forma = 0, n = 1):
+def kernel_rechazabandas(M, N, c0 = 20, w = 10, forma = 0, n = 1):
     """forma = 0 para kernel ideal, 1 para gaussiano y cualquier otro para Butterworth."""
-    M, N = imagen.shape
     V, U = hacer_meshgrid(M, N)
     # Distancias al cuadrado
     D = np.square(U - 0.5 * M) + np.square(V - 0.5 * N)
     
     if forma == 0:
-        provisional1 = np.where(D >= c0 - 0.5 * w, 1, 0)
-        provisional2 = np.where(D >= c0 + 0.5 * w, 1, 0)
-        provisional3 = provisional1 + provisional2
+        provisional1 = np.where(D <= c0 - 0.5 * w, 1, 0)
+        provisional2 = np.where(D <= c0 + 0.5 * w, 1, 0)
+        provisional3 = provisional2 - provisional1
         kernel = np.where(provisional3 == 0, 1, 0)
     elif forma == 1:
         kernel = 1.0 - np.exp(-((D - c0**2) / (w * (D + 1e-6)))**2)
@@ -71,3 +69,89 @@ def kernel_rechazabandas(imagen, c0 = 20, w = 10, forma = 0, n = 1):
         kernel = np.where(D != c0**2, 1.0 / (1.0 + ((D * w**2) / (D + 1e-6 - c0**2))**n), 0.0)
     
     return kernel
+
+def kernel_pasabandas(M, N, c0 = 20, w = 10, forma = 0, n = 1):
+    """forma = 0 para kernel ideal, 1 para gaussiano y cualquier otro para Butterworth."""
+    kernel = 1.0 - kernel_rechazabandas(M, N, c0, w, forma, n)
+    
+    return kernel
+
+ # Filtros notch
+def kernel_ideal_notch(M, N, centro, d0):
+    u_k = centro[0]
+    v_k = centro[1]
+    V, U = hacer_meshgrid(M, N)
+    
+    D_k = np.square(U - 0.5 * M - u_k) + np.square(V - 0.5 * N - v_k)
+    D_mk = np.square(U - 0.5 * M + u_k) + np.square(V - 0.5 * N + v_k)
+    H_k = np.where(D_k <= d0**2, 0, 1) # Primer pasaaltos
+    H_mk = np.where(D_mk <= d0**2, 0, 1) # Segundo pasaaltos
+    kernel = H_k * H_mk
+    
+    return kernel
+
+def kernel_gaussiano_notch(M, N, centro, d0):
+    u_k = centro[0]
+    v_k = centro[1]
+    V, U = hacer_meshgrid(M, N)
+    
+    D_k = np.square(U - 0.5 * M - u_k) + np.square(V - 0.5 * N - v_k)
+    D_mk = np.square(U - 0.5 * M + u_k) + np.square(V - 0.5 * N + v_k)
+    H_k = 1 - np.exp(-(0.5 / d0**2) * D_k) # Primer pasaaltos
+    H_mk = 1 - np.exp(-(0.5 / d0**2) * D_mk) # Segundo pasaaltos
+    kernel = H_k * H_mk
+    
+    return kernel
+
+def kernel_butterworth_notch(M, N, centro, d0, n):
+    u_k = centro[0]
+    v_k = centro[1]
+    V, U = hacer_meshgrid(M, N)
+    
+    D_k = np.square(U - 0.5 * M - u_k) + np.square(V - 0.5 * N - v_k)
+    D_mk = np.square(U - 0.5 * M + u_k) + np.square(V - 0.5 * N + v_k)
+    H_k = np.divide(D_k**n, D_k**n + d0**(2*n)) # Primer pasaaltos
+    H_mk = np.divide(D_mk**n, D_mk**n + d0**(2*n)) # Segundo pasaaltos
+    kernel = H_k * H_mk
+    
+    return kernel
+
+def kernel_notch(M, N, d0, centro = (0, 0), forma = 0, pasa = 0, n = 1.0):
+    """Filtro notch. 
+    forma = 0 para ideal, 1 para gaussiano y cualquier otro valor para butterworth.
+    pasa = 0 para notchreject, 1 para notchpass.
+    centro y radio son los del notch. notch simétrico automático.
+    Especificar n solo para butterworth"""
+    
+    if forma == 0:
+        kernel_prov = kernel_ideal_notch(M, N, centro, d0)
+    elif forma == 1:
+        kernel_prov = kernel_gaussiano_notch(M, N, centro, d0)
+    else:
+        kernel_prov = kernel_butterworth_notch(M, N, centro, d0, n)
+        
+    kernel = pasa + (-1)**pasa * kernel_prov
+    
+    return kernel
+
+def kernel_maestro(M, N, d0 = 15, centro = (0,0), pasa = 0, n = 1, c0 = 20, w = 10, tipo = 'pasabajos', forma = 0):
+    """Me parece que lo mejor es llamar a los argumentos con todo y nombre.
+    Usar solo strings permitidos para el argumento 'tipo' o no me hago responsable :3"""
+    if tipo == 'pasabajos':
+        kernel = kernel_pasabajos(M, N, d0, forma, n)
+    elif tipo == 'pasaaltos':
+        kernel = kernel_pasaaltos(M, N, d0, forma, n)
+    elif tipo == 'pasabandas':
+        kernel = kernel_pasabandas(M, N, c0, w, forma, n)
+    elif tipo == 'rechazabandas':
+        kernel = kernel_rechazabandas(M, N, c0, w, forma, n)
+    else: # Asume notch
+        kernel = kernel_notch(M, N, d0, centro, forma, pasa, n)
+        
+    return kernel
+
+def filtrar(imagen, d0 = 15, centro = (0,0), pasa = 0, n = 1, c0 = 20, w = 10, tipo = 'pasabajos', forma = 0):
+    M, N = imagen.shape
+    kernel = kernel_maestro(M, N, d0, centro, pasa, n, c0, w, tipo, forma)
+    
+    return filtros.aplicar_filtro(imagen, kernel)
